e9ec7664a5489948909c92c8347e94c0a04700b7
ramonfragoso <ramon.fragoso@mutual.club>
2021-12-15 00:57:19 -0300

    upload full project

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def contact
+
+  end
+
+  def about
+    @title = 'My cool page'
+  end
+
+  def home
+    @projects = Project.all
+  end
+
+  def error
+    
+  end
+end
